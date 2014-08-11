7a274541aaedf77143952c4ae8b214f97dc008fb
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-11 00:50:04 -0400

    added static files

--- editor/settings.py
+++ editor/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/Users/ben/code/ipoe/editor/static/'
+STATIC_ROOT = '/var/www/ipoe.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -73,6 +73,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/ben/code/ipoe/editor/static',
 )
 
 # List of finder classes that know how to find static files in
