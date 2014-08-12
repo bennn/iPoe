944d725d3adb1226c3a9cdd7d00e38f55618afb4
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-12 18:44:27 -0400

    print dummy error message

--- editor/views.py
+++ editor/views.py
@@ -1,14 +1,23 @@
 import poem
 
+from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 
 
 def home(request):
     return render_to_response('index.html', {})
 
-def editor(request, poem_type=None):
-    poem_class = poem.of_string(poem_type)
-    cookie = {
-        'poem': poem_class()
-    }
-    return render_to_response('editor.html', cookie)
+def editor(request, poem_slug=None):
+    cookie = {}
+    cookie.update(csrf(request)) # Required for csrf form protection
+    cookie['slug'] = poem_slug
+    poem_class = poem.of_string(poem_slug)
+    if request.method == "POST":
+        # Compile poem
+        cookie['poem'] = poem_class(request.POST.get('poem_content', ''))
+        cookie['error_message'] = cookie['poem'].compile()
+        return render_to_response('editor.html', cookie)
+    else:
+        # Prepare to create poem
+        cookie['poem'] = poem_class()
+        return render_to_response('editor.html', cookie)
