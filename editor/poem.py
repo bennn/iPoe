3d87f325dc934d184f0086b12f7abcf63c86f909
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-12 16:54:05 -0400

    basic poem classes

--- editor/poem.py
+++ editor/poem.py
@@ -1,14 +1,18 @@
 class Poem:
 
+    NAME               = "???"
     NUM_LINES          = None
     NUM_STANZAS        = None
     LINES_PER_STANZA   = []
     SYLLABLES_PER_LINE = []
 
-    def get_numlines():
+    def get_name(self):
+        return self.NAME
+
+    def get_numlines(self):
         return self.NUM_LINES
 
-    def get_numstanzas():
+    def get_numstanzas(self):
         return self.NUM_STANZAS
 
     def lines_of_stanza(self, stanza_index):
@@ -23,11 +27,20 @@ class Poem:
         else:
             return None
 
-class Free(Poem):
-    pass
+class FreeVerse(Poem):
+    NAME = "Free Verse"
 
 class Haiku(Poem):
+    NAME               = "Haiku"
     NUM_LINES          = 3
     NUM_STANZA         = 1
     LINES_PER_STANZA   = [3]
     SYLLABLES_PER_LINE = [5,7,5]
+
+def of_string(poem_type):
+    if poem_type == "free":
+        return FreeVerse
+    elif poem_type == "haiku":
+        return Haiku
+    else: # default / not-implemented
+        return Poem
