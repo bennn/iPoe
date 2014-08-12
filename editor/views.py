d9a727edd9fdd935d2e41be4636a6d4049dc8a75
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-12 17:07:07 -0400

    simple editor view

--- editor/views.py
+++ editor/views.py
@@ -1,7 +1,14 @@
+import poem
+
 from django.shortcuts import render_to_response
+
 
 def home(request):
     return render_to_response('index.html', {})
 
-def editor(request, poemtype=""):
-    return render_to_response('index.html', {})
+def editor(request, poem_type=None):
+    poem_class = poem.of_string(poem_type)
+    cookie = {
+        'poem': poem_class()
+    }
+    return render_to_response('editor.html', cookie)
