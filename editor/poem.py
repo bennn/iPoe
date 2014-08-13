393fabc2aa894966d4793545da0c7120cdfd2ad2
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-13 09:38:07 -0400

    villanelle, rhyme scheme drafts

--- editor/poem.py
+++ editor/poem.py
@@ -20,7 +20,12 @@ class Poem:
 
     def compile(self):
         # Compare 'self.content' to an expected poem of this format. Default is 'PASS!'
-        # TODO break into methods?
+        # TODO break into methods. What does it mean for a poem to be correct?
+        # right number of lines
+        # right number of stanzas
+        # right number of lines per stanza
+        # right number of syllables per line
+        # right rhyme scheme (can I do schemes as lists? [a, b, a, b, c, c])
         return None
 
     def get_name(self):
@@ -50,12 +55,65 @@ class FreeVerse(Poem):
 class Haiku(Poem):
     NAME               = "Haiku"
     NUM_LINES          = 3
-    NUM_STANZA         = 1
+    NUM_STANZAS        = 1
     LINES_PER_STANZA   = [3]
     SYLLABLES_PER_LINE = [5,7,5]
 
     def compile(self):
         return "Errors everywhere"
+
+class Rhyme:
+    SOUND = None
+    def __init__(self, word):
+        # TODO get the ending sound from the word
+        self.SOUND = None
+        
+    def match(self, sound):
+        # Check if argument sound matches this rhyme
+        if self.SOUND is None:
+            # Set sound
+            self.SOUND = sound
+        # Check sound
+        return self.SOUND == sound
+
+class Refrain:
+    WORDS = None
+    RHYME = None
+
+    def __init__(self, rhyme):
+        self.RHYME = rhyme
+
+    def match(self, words):
+        # Check if that line matches this line. Same words.
+        return self.WORDS == words
+
+    def set(self, words):
+        last_word = Word(words.rsplit(" ", 1)[-1])
+        if self.RHYME == Rhyme(last_word):
+            self.WORDS = words
+            return True
+        else:
+            return False
+
+class Villanelle(Poem):
+    NAME               = "Villanelle"
+    NUM_LINES          = 19
+    NUM_STANZAS        = 6
+    LINES_PER_STANZA   = [3, 3, 3, 3, 3, 4]
+    SYLLABLES_PER_LINE = [10] * 19
+
+    a  = Rhyme()
+    b  = Rhyme()
+    r1 = Refrain(a)
+    r2 = Refrain(a)
+    RHYME_SCHEME = [r1, b, r2,
+                    a,  b, r1,
+                    a,  b, r2,
+                    a,  b, r1,
+                    a,  b, r2,
+                    a,  b, r1, r2]
+                        
+
 
 def of_string(poem_type):
     if poem_type == "free":
