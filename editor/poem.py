cc4120cc0e437fb3b722d79662e5250001944e6d
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-13 21:02:30 -0400

    poem descriptions

--- editor/poem.py
+++ editor/poem.py
@@ -1,74 +1,9 @@
-class Poem:
-
-    NAME               = "???"
-    NUM_LINES          = None
-    NUM_STANZAS        = None
-    LINES_PER_STANZA   = []
-    SYLLABLES_PER_LINE = []
-
-    def __init__(self, content=""):
-        self.content = content
-
-    def as_text_area(self):
-        # render the widget to html
-        fmt = {
-            'content'  : self.content,
-            'num_rows' : (self.NUM_LINES or 35) + (self.NUM_STANZAS or 0),
-            'num_cols' : 80,
-        }
-        return """<textarea id='poem_content' name='poem_content' rows='{num_rows}' cols='{num_cols}'>{content}</textarea>""".format(**fmt)
-
-    def compile(self):
-        # Compare 'self.content' to an expected poem of this format. Default is 'PASS!'
-        # TODO break into methods. What does it mean for a poem to be correct?
-        # right number of lines
-        # right number of stanzas
-        # right number of lines per stanza
-        # right number of syllables per line
-        # right rhyme scheme (can I do schemes as lists? [a, b, a, b, c, c])
-        return None
-
-    def get_name(self):
-        return self.NAME
-
-    def get_numlines(self):
-        return self.NUM_LINES
-
-    def get_numstanzas(self):
-        return self.NUM_STANZAS
-
-    def lines_of_stanza(self, stanza_index):
-        if stanza_index < len(self.LINES_PER_STANZA):
-            return self.LINES_PER_STANZA[stanza_index]
-        else:
-            return None
-
-    def syllables_of_line(self, line_index):
-        if line_index < len(self.SYLLABLES_PER_LINE):
-            return self.SYLLABLES_PER_LINE[line_index]
-        else:
-            return None
-
-class FreeVerse(Poem):
-    NAME = "Free Verse"
-    # descr = free verse has no rules. write whatever you like #poetic_license
-
-class Haiku(Poem):
-    NAME               = "Haiku"
-    NUM_LINES          = 3
-    NUM_STANZAS        = 1
-    LINES_PER_STANZA   = [3]
-    SYLLABLES_PER_LINE = [5,7,5]
-    # descr = traditional haikus consist of 3 lines with 5 syllables on the first line, 7 syllables on the second, and 5 syllables on the third.
-    #         the secret of the best haikus is juxtaposition: they combine two very different ideas or feelings.
-
-    def compile(self):
-        return "Errors everywhere"
-
 class Rhyme:
     SOUND = None
-    def __init__(self, word):
+    def __init__(self, word=None):
         # TODO get the ending sound from the word
+        if word is not None:
+            pass
         self.SOUND = None
         
     def match(self, sound):
@@ -98,15 +33,108 @@ class Refrain:
         else:
             return False
 
+class Poem:
+
+    NAME               = "???"
+    NUM_LINES          = None
+    NUM_STANZAS        = None
+    LINES_PER_STANZA   = []
+    SYLLABLES_PER_LINE = []
+
+    def __init__(self, content=""):
+        self.content = content
+
+    def as_text_area(self):
+        # render the widget to html
+        fmt = {
+            'content'  : self.content,
+            'num_rows' : (self.NUM_LINES or 35) + (self.NUM_STANZAS or 0),
+            'num_cols' : 80,
+        }
+        return """<textarea id='poem_content' name='poem_content' rows='{num_rows}' cols='{num_cols}'>{content}</textarea>""".format(**fmt)
+
+    def compile(self):
+        # Compare 'self.content' to an expected poem of this format. Default is 'PASS!'
+        # TODO break into methods. What does it mean for a poem to be correct?
+        # right number of lines
+        # right number of stanzas
+        # right number of lines per stanza
+        # right number of syllables per line
+        # right rhyme scheme (can I do schemes as lists? [a, b, a, b, c, c])
+        # other stuff (limerick should have fewer syllables in lines 3,4
+        return None
+
+    def get_description(self):
+        return "There's no description."
+
+    def get_name(self):
+        return self.NAME
+
+    def get_numlines(self):
+        return self.NUM_LINES
+
+    def get_numstanzas(self):
+        return self.NUM_STANZAS
+
+    def lines_of_stanza(self, stanza_index):
+        if stanza_index < len(self.LINES_PER_STANZA):
+            return self.LINES_PER_STANZA[stanza_index]
+        else:
+            return None
+
+    def syllables_of_line(self, line_index):
+        if line_index < len(self.SYLLABLES_PER_LINE):
+            return self.SYLLABLES_PER_LINE[line_index]
+        else:
+            return None
+
+class FreeVerse(Poem):
+    NAME = "Free Verse"
+
+    def get_description(self):
+        return "Free verse has no rules. Go ham."
+
+class Haiku(Poem):
+    NAME               = "Haiku"
+    NUM_LINES          = 3
+    NUM_STANZAS        = 1
+    LINES_PER_STANZA   = [3]
+    SYLLABLES_PER_LINE = [5,7,5]
+
+    def compile(self):
+        return "Errors everywhere"
+
+    def get_description(self):
+        return " ".join(("Traditional haikus consist of 3 lines with 5 syllables on the first line, 7 syllables on the second, and 5 syllables on the third.",
+            "Juxtaposition is nice for haikus.",
+            "Combine two very different ideas or feelings with a key 'cutting' word."))
+    
+class Limerick(Poem):
+    NAME               = "Limerick"
+    NUM_LINES          = 5
+    NUM_STANZAS        = 1
+    LINES_PER_STANZA   = [5]
+    SYLLABLES_PER_LINE = [] # roughly 8, 8, 6, 6, 8, but too rough to call
+
+    a  = Rhyme()
+    b  = Rhyme()
+    RHYME_SCHEME = [a, a, b, b, a]
+
+    def check_other(self):
+        # Coreectness = right rhyme. It should taste like a song, but not sure how to check that
+        return True
+    
+    def get_description(self):
+        return "Limericks are fun."
+
 class Villanelle(Poem):
     NAME               = "Villanelle"
     NUM_LINES          = 19
     NUM_STANZAS        = 6
     LINES_PER_STANZA   = [3, 3, 3, 3, 3, 4]
-    SYLLABLES_PER_LINE = [10] * 19
+    SYLLABLES_PER_LINE = [10] * NUM_LINES
 
-    a  = Rhyme()
-    b  = Rhyme()
+    (a,b)  = Rhyme(), Rhyme()
     r1 = Refrain(a)
     r2 = Refrain(a)
     RHYME_SCHEME = [r1, b, r2,
@@ -115,13 +143,50 @@ class Villanelle(Poem):
                     a,  b, r1,
                     a,  b, r2,
                     a,  b, r1, r2]
-    # correct = right lines, right stanzas, right syllables, right rhyme scheme
-    # descr = A villanelle is a 19-line, iambic pentameter poem broken into 6 stanzas.
-    #         The first stanza sets the refrain.
-    #         Its first line must be the last line of stanzas 2 and 4, and the second-to-last line of the final stanza.
-    #         Its last line must be the last line of stanzas 3, 5, and 6.
-    #         These first and last lines must rhyme both with each other and with the first line of every proceeding stanza.
-    #         Additionally, the second line of every stanza must rhyme.
+    def get_description(self):
+        return " ".join(("A villanelle is a 19-line, iambic pentameter poem broken into 6 stanzas.",
+            "The first stanza sets the refrain.",
+            "Its first line must be the last line of stanzas 2 and 4, and the second-to-last line of the final stanza.",
+            "Its last line must be the last line of stanzas 3, 5, and 6.",
+            "These first and last lines must rhyme both with each other and with the first line of every proceeding stanza.",
+            "Additionally, the second line of every stanza must rhyme."))
+            # "There seems to be a connection between villanelles and obsession."
+            # "See Dylan Thomas or Sylvia Plath."
+
+class ItalianSonnet(Poem):
+    NAME               = "Italian (Petrarchan) Sonnet"
+    NUM_LINES          = 14
+    NUM_STANZAS        = 1
+    LINES_PER_STANZA   = [NUM_LINES]
+    SYLLABLES_PER_LINE = [10] * NUM_LINES
+
+    (a,b,c,d)  = (Rhyme(), Rhyme(), Rhyme(), Rhyme())
+    RHYME_SCHEME = [a, b, b, a,
+                    a, b, b, a,
+                    c, d, e, c, d, e]
+
+    def get_description(self):
+        return " ".join(("The Italian sonnet, invented by Giacomo de Lentini in the 1200s, consists of 14-lines of iambic pentameter.",
+            "The first eight lines are usually the introduction and the last six are usually the resolution.",
+            "Rhyme scheme is 'abba abba cde cde'."))
+
+class EnglishSonnet(Poem):
+    NAME               = "English (Shakespearian) Sonnet"
+    NUM_LINES          = 14
+    NUM_STANZAS        = 1
+    LINES_PER_STANZA   = [14]
+    SYLLABLES_PER_LINE = [10] * NUM_LINES
+
+    (a,b,c,d,e,f,g)  = (Rhyme(), Rhyme(), Rhyme(), Rhyme(), Rhyme(), Rhyme(), Rhyme())
+    RHYME_SCHEME = [a, b, a, b,
+                    c, d, c, d,
+                    e, f, e, f,
+                    g, g]
+
+    def get_description(self):
+        return " ".join(("English sonnets are 14 lines in iambic pentameter.",
+            "The rhyme scheme is 'abab cdcd efef gg'.",
+            "Notice that couplet at the end."))
 
 
 def of_string(poem_type):
@@ -129,5 +194,13 @@ def of_string(poem_type):
         return FreeVerse
     elif poem_type == "haiku":
         return Haiku
+    elif poem_type == "villanelle":
+        return Villanelle
+    elif poem_type == "limerick":
+        return Limerick
+    elif poem_type == "italian-sonnet":
+        return ItalianSonnet
+    elif poem_type == "english-sonnet":
+        return EnglishSonnet
     else: # default / not-implemented
         return Poem
