9d211c4abf5b4e704cfa956d27d96660de02578f
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-16 21:39:31 -0400

    finished crawler script

--- editor/dictionary/scrape/scrape-syllables-and-rhymes.py
+++ editor/dictionary/scrape/scrape-syllables-and-rhymes.py
@@ -1,14 +1,25 @@
 import os
-import re
 import sys
-import urllib
+import time
+import random
 
-from dictionary.scrape import util
-# from scrapy.selector import HtmlXPathSelector
+import util
+
+OUTPUT_DIRECTORY = "./output"
+
+out1 = "%s/word-syllables.tab" % OUTPUT_DIRECTORY
+out2 = "%s/word-rhymes.tab"    % OUTPUT_DIRECTORY
+
+def get_download_delay():
+    v1 = random.randint(0,15)
+    if random.randint(0,1):
+        return v1
+    else:
+        return v1 + random.randint(5, 30)
 
 def parse_hyphenated(hyphen):
     # Make sure there are no spaces in these words! Else we just ignore them.
-    chars, num_syllables = [], 0
+    chars, num_syllables = [], 1
     for c in hyphen:
         if ord(c) == 165:
             # That's a hyphen!
@@ -35,6 +46,7 @@ def run(fname):
                         print("Processing hyphenated word '%s'..." % hyphen)
                         word, syllables       = parse_hyphenated(hyphen)
                         rhymes, almost_rhymes = util.scrape_rhymebrain(word)
+                        time.sleep(get_download_delay())
                         print>>out_syll,  ("%s\t%s" % (word, syllables))
                         print>>out_rhyme, ("%s\t%s\t%s" % (word, ",".join(rhymes), ",".join(almost_rhymes)))
                         print("Scraped word '%s' with '%s' syllables '%s' rhymes, and '%s' almost rhymes." % (word, syllables, len(rhymes), len(almost_rhymes)))
@@ -42,6 +54,8 @@ def run(fname):
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
+        if not (os.path.exists(OUTPUT_DIRECTORY)):
+            os.mkdir(OUTPUT_DIRECTORY)
         run(sys.argv[1])
     else:
         print("usage: scrape-syllables-and-rhymes <filename>")
