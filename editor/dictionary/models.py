cc92022e101664e68a72cdfada95e3739185ec28
Ben Greenman <greenman.b@husky.neu.edu>
2014-08-18 00:54:08 -0400

    revised word class

--- editor/dictionary/models.py
+++ editor/dictionary/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class Word(models.Model):
-    name = models.CharField(max_length=50)
-    num_syllables = models.IntegerField()
+    name               = models.CharField(max_length=50)
+    num_syllables      = models.IntegerField()
+    rhymes_with        = models.ManyToManyField(Word)
+    almost_rhymes_with = models.ManyToManyField(Word)
 
     def __unicode__(self):
         return self.name
