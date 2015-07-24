791acf6f0e93616e11db14c24edc1763f20e1cbf
ben <types@ccs.neu.edu>
2015-07-24 04:29:34 -0400

    [migrate] sync db script

---
+++ racket/migrate/sync.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+;; Perform database migrations in this folder (i.e., XXXX.sql files)
+;; Use the file `VERSION` to remember the current version number.
+;; Raise an error if version (i+1) is missing.
+
+(require db glob)
+
+;; =============================================================================
+
+(define VERSION "./version.rktd")
+
+(define (get-current-version)
+  (file->value VERSION))
+
+(define (set-current-version n)
+  (with-output-to-file VERSION #:exists 'replace
+    (lambda () (displayln n))))
+
+(define (filename->number fname)
+  (call-with-exception-handler
+    (lambda (err) (raise-user-error 'sync (format "Could not parse number from filename '~a'" fname)))
+    (lambda () (string->number (car (string-split (cadr (string-split fname "/")) "."))))))
+
+(define (migrate pgc fname)
+  (query-exec pgc (file->string fname)))
+
+(define (main)
+  (define init-version (get-current-version))
+  (define pgc (postgresql-connect #:user "ben" #:database "ipoe"))
+  (define new-version
+    (for/fold ([version init-version])
+              ([fname (in-glob "./*.sql")])
+      (define i (filename->number fname))
+      (define v+1 (add1 version))
+      (cond
+        [(<= i version)
+         ;(printf "[SYNC] skipping old file '~a'\n" fname)
+         version]
+        [(= i v+1)
+         (printf "[SYNC] executing '~a'\n" fname)
+         (migrate pgc fname)
+         (set-current-version v+1)
+         v+1]
+        [else
+         (raise-user-error 'sync (format "missing version '~a'" (add1 version)))])))
+  (printf "[SYNC] successfully performed ~a updates\n" (- new-version init-version))
+  (set-current-version new-version))
+
+;; =============================================================================
+
+(module+ main
+  (main))
+
+;; =============================================================================
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (filename->number "./001.sql") 1)
+  (check-equal? (filename->number "./912") 912)
+  (check-equal? (filename->number "./0200.sql") 200)
+)
