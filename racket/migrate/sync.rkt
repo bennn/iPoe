ac0cfd278c69869a2811ab62fb67377eda05891a
ben <types@ccs.neu.edu>
2015-08-01 13:38:38 -0400

    [racket] added rhymes + almost-rhymes data

--- racket/migrate/sync.rkt
+++ racket/migrate/sync.rkt
@@ -10,24 +10,34 @@
 
 (define VERSION "./version.rktd")
 
+;; Read current version number from the file
 (define (get-current-version)
   (file->value VERSION))
 
+;; Update the current version number
 (define (set-current-version n)
   (with-output-to-file VERSION #:exists 'replace
     (lambda () (displayln n))))
 
+;; Convert an `XXXX.sql` filename to a number.
+;; By convention, these filenames are 4-digit integers padded with zeros to the left.
 (define (filename->number fname)
   (call-with-exception-handler
     (lambda (err) (raise-user-error 'sync (format "Could not parse number from filename '~a'" fname)))
     (lambda () (string->number (car (string-split (cadr (string-split fname "/")) "."))))))
 
+;; Execute a query on the database.
+;; TODO query-exec gives poor error messages
+;; TODO this is very inefficient for large files, I had better luck running
+;;      `psql -U <user> -d <database> -a -f <file.sql>` manually.
 (define (migrate pgc fname)
   (query-exec pgc (file->string fname)))
 
 (define (main)
+  ;; -- get current version, connect to database
   (define init-version (get-current-version))
   (define pgc (postgresql-connect #:user "ben" #:database "ipoe"))
+  ;; -- fold over all version files, execute new _successors_ only
   (define new-version
     (for/fold ([version init-version])
               ([fname (in-glob "./*.sql")])
