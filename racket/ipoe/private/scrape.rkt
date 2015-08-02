4b25a8db965f8246ff447e805dd8a7b26538cf19
ben <types@ccs.neu.edu>
2015-08-02 02:11:53 -0400

    [racket/scrape] outlined files, so it compiles

---
+++ racket/ipoe/private/scrape.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+
+(provide
+  resolve-rhyme*
+  resolve-almost-rhyme*
+  resolve-syllables
+)
+
+(require
+  "scrape/scrape-rhymes.rkt"
+  "scrape/scrape-syllables.rkt"
+)
---
+++ racket/ipoe/private/scrape/scrape-syllables.rkt
@@ -0,0 +1,13 @@
+#lang racket/base
+
+(provide
+  resolve-syllables
+)
+
+;; =============================================================================
+
+;; Validate the suggested number of syllables for a word
+;; (-> String (U Natural #f) Natural)
+(define (resolve-syllables word syllables)
+  (error 'scrape-syllables "not implemented"))
+
