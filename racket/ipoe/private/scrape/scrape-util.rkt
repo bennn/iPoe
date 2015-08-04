751c9d7f6aca1c6f2a0a0a88cf22717c5cdf3895
ben <types@ccs.neu.edu>
2015-08-04 03:15:49 -0400

    [racket/scrape] added more filters

--- racket/ipoe/private/scrape/scrape-util.rkt
+++ racket/ipoe/private/scrape/scrape-util.rkt
@@ -6,14 +6,36 @@
   ;; Count the number of characters satisfying the predicate
 
   url->html
+  ;; (-> (U url String) html)
+  ;; Get the html data that lives at location URL
 
   url->sxml
+  ;; (-> (U url String) sxml)
+  ;; Get the html data that lives at location URL and parse it to sxml
+
+  ;; -- filters
+  id?
+  ;; (-> String (-> (Listof sxml) ??? (Listof sxml)))
+  ;; Given a string `str`, create an sxml filter that accepts all elements
+  ;;  whose id attribute matches `str` exactly.
+
+  class?
+  ;; (-> String (-> (Listof sxml) ??? (Listof sxml)))
+  ;; Given a string `str`, create an sxml filter that accepts all elements
+  ;;  whose class attribute matches `str` exactly.
+
+  contains-text?
+  ;; (-> String (-> (Listof sxml) ??? (Listof sxml)))
+  ;; Given a string `str`, create an sxml filter that accepts all elements
+  ;;  whose text attribute matches the regular expression pattern `str`.
+
 )
 
 (require
   net/url
   (only-in html read-html)
   (only-in "html.rkt" html->xexp)
+  sxml
 )
 
 ;; =============================================================================
@@ -35,3 +57,24 @@
    [(string? arg) (string->url arg)]
    [(path? arg)   (path->url arg)]
    [(symbol? arg) (string->url (symbol->string arg))]))
+
+;; Filter elements that do not have an id attribute equal to `str`
+(define ((id? str) elem* ???)
+  (define (id=str? e)
+     (define id-text ((if-car-sxpath '( @ id *text*)) e))
+     (and id-text (string=? str id-text)))
+  (filter id=str? elem*))
+
+(define ((class? str) elem* ???)
+  (define (class=str? e)
+    (define class-text ((if-car-sxpath '(@ class *text*)) e))
+    (and class-text (string=? str class-text)))
+  (filter class=str? elem*))
+
+;; Filter elements that do not contain text matching the pattern
+(define ((contains-text? pat) elem* ???)
+  (define (contains? e)
+    (define txt ((if-car-sxpath '(*text*)) e))
+    (and txt (regexp-match pat txt)))
+  (filter contains? elem*))
+
