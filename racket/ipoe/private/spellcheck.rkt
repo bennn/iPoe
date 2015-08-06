0be78c9a6a2b05624ac64996a5fe39abc1321019
ben <types@ccs.neu.edu>
2015-08-06 03:49:23 -0400

    [racket] abstracted spellcheck function

--- racket/ipoe/private/spellcheck.rkt
+++ racket/ipoe/private/spellcheck.rkt
@@ -3,8 +3,12 @@
 ;; A very simple spellchecker
 
 (provide
-  ;; (-> (-> String Boolean))
+  check-spelling
+  ;; (-> (Sequenceof String) Void)
+  ;; Check all words in a sequence of lines for spelling errors
+
   spellchecker
+  ;; (-> (-> String Boolean))
 )
 
 ;; -----------------------------------------------------------------------------
@@ -12,9 +16,30 @@
 (require
   ipoe/private/db
   ipoe/private/parse
+  ipoe/private/ui
+  (only-in racket/string string-split)
   (only-in racket/file file->value))
 
 ;; =============================================================================
+;; Is there an interesting tradeoff between parameters & closing connections?
+(define *pgc* (make-parameter #f))
+
+;; Always succeeds.
+;; As of 2015-08-06, just makes an alert if words are mispelled
+;; - doesn't raise an error
+;; - doesn't offer suggestions
+;; (: check-spelling (-> (Sequenceof String) Void))
+(define (check-spelling line*)
+  (unless (*pgc*) (*pgc* (db-init)))
+  (for ([line line*]
+        [line-num (in-naturals)])
+    (for ([word (in-list (string-split line))]
+          [word-num (in-naturals)])
+      (define normalized (parse-word word))
+      ;; Do nothing for non-words (punctuation)
+      (when normalized
+        (unless (word-exists? (*pgc*) normalized)
+          (alert (format "Warning: mispelled word '~a' on line '~a'" word line-num)))))))
 
 ;; Create a spellchecking function that references the ipoe database
 (define (spellchecker)
