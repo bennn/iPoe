6a2f2071b876a3235e1cc8f375f3d381d45cd513
ben <types@ccs.neu.edu>
2015-08-14 23:43:17 -0400

    [reader/langs] updated existing languages with new specs, looks great

--- racket/ipoe/haiku/lang/reader.rkt
+++ racket/ipoe/haiku/lang/reader.rkt
@@ -1,43 +1,8 @@
-#lang racket/base
+#lang s-exp ipoe/reader
 
-(provide
-  (rename-out
-    [read/haiku read]
-    [read-syntax/haiku read-syntax])
-  haiku)
-
-;; -----------------------------------------------------------------------------
-
-(require
-  ipoe/private
-  syntax/strip-context
-  (only-in racket/string string-join)
-  (only-in racket/sequence sequence->list)
-)
-
-;; =============================================================================
-
-(define (haiku arg)
-  (define line* (to-line* arg))
-  (define stanza* (sequence->list (to-stanza* line*)))
-  ;; Fail if rhyme scheme doesn't match
-  (assert-success #:src 'haiku
-    (check-rhyme-scheme stanza* #:rhyme-scheme '(((* . 5) (* . 7) (* . 5)))))
-  ;; Post a warning if spelling fails
-  (on-failure (check-spelling line*)
-    (lambda (fl) (alert (failure-reason fl))))
-  (string-join line* "\n"))
-
-(define (read/haiku in)
-  (syntax->datum (read-syntax/haiku #f in)))
-
-(define (read-syntax/haiku src-path in)
-  (with-syntax ([str (haiku in)])
-    (strip-context
-      #`(module anything racket
-          (provide data)
-          (define data 'str)))))
-
-(define (make-info key default use-default)
-  (use-default key default))
-
+;; One stanza,
+;; no rhyme scheme
+;; 5 syllables in first line
+;; 7 in second
+;; 5 in last
+'(((* . 5) (* . 7) (* . 5)))
