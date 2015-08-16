863e782c2c8d10095c8301ac8000183071a44e66
ben <types@ccs.neu.edu>
2015-08-15 21:23:52 -0400

    [racket/langs] simple villanelle reader, does not check extra-constraint

---
+++ racket/ipoe/villanelle/lang/reader.rkt
@@ -0,0 +1,10 @@
+#lang ipoe
+
+#:name villanelle
+#:rhyme-scheme ((R1 B R2)
+                (A  B R1)
+                (A  B R2)
+                (A  B R1)
+                (A  B R2)
+                (A  B R1 R2))
+#:syllables 10
