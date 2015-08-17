973658fa2f63d9f0e951223e483b5106eab2bd84
ben <types@ccs.neu.edu>
2015-08-17 13:58:23 -0400

    [racket/langs] add limerick

---
+++ racket/examples/limerick/clean.rkt
@@ -0,0 +1,7 @@
+#lang ipoe/limerick
+
+the limerick packs laughs anatomical
+into space that is quite economical
+but the good ones I've seen
+so seldom are clean
+and the clean ones so seldom are comical
