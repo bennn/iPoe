33676a635285879f4715d33f7d4e4e0f963f0f1d
ben <types@ccs.neu.edu>
2015-08-17 14:14:31 -0400

    [racket/langs] add rondelet

---
+++ racket/examples/rondelet/happy.rkt
@@ -0,0 +1,9 @@
+#lang ipoe/rondelet
+
+Happy am I!   TODO syllables for one of these words is wrong.
+For I have found a friend in you
+Happy am I!
+The sun is shining in the sky.
+I know that you will see me through,
+No more will I be feeling blue.
+Happy am I!
