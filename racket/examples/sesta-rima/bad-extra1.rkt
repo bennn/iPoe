7a31f709abafcf0d770e68017d01413dcd140e83
ben <types@ccs.neu.edu>
2015-08-24 14:46:35 -0400

    [cerror] checkpoint: for/and doesn't handle the Either

---
+++ racket/examples/sesta-rima/bad-extra1.rkt
@@ -0,0 +1,47 @@
+#lang ipoe/sesta-rima
+
+It raining on black coal and warm wet ashes.
+There are tyre-marks in the yard, Agnew's old lorry
+Has all its cribs down and Agnew the coalman
+With his Belfast accent's sweet-talking my mother.
+Would she ever go to a film in Magherafelt?
+But it's raining and he still has half the load
+
+To deliver farther on. This time the load
+Our coal came from was silk-black, so the washes
+Will be the silkiest white. The Magherafelt
+(Via Toomebridge) bus goes by. The half-stripped sorry
+With its emptied, folded coal-bags moves my mother:
+The tasty ways of a leather-aproned coalman!
+
+And films no less! The conceit of a coalman ...
+She goes back in and gets out the black load
+And emery paper, this nineteen-forties mother,
+All business round her stove, half-wiping ashes
+With a backhand from her cheek as the bolted lorry
+Gets revved and turned and heads for Magherafelt
+
+And the last delivery. Oh, Magherafelt!
+Oh, dream of red plush and a city coalman
+As time fastforwards and a different lorry
+Groans into shot, up Broad Street, with a load
+That will blow the bus station to dust and ashes...
+After that happened, I'd a vision of my mother,
+
+A revenant on the bench where I would meet mother
+In that cold-floored waiting room in Magherafelt,
+Her shopping bags full up with shovelled ashes.
+Death walked out past her like a dust-faced coalman
+Refolding body-bags, plying his load
+Empty upon empty, in a lorry
+
+Of motes and engine-revs, but which lorry
+Was it now? Young Agnew's or that mother,
+Heavier, deadlier one, set to load
+In a time beyond her time in Magherafelt...
+So tally bags and sweet-talk darkness, coalman,
+Listen to the rain spit in new ashes
+
+As you heft a lorry of dust that was Magherafelt,
+Then reappear from your mother as my coalman
+Dreamboat load filmed in silk-white ashes.
