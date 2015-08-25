b5ce37161ab8faa23b8e84b6ea775afb062d6c65
ben <types@ccs.neu.edu>
2015-08-25 02:34:11 -0400

    [cerror] sesta-rima validator really catching errors

--- racket/ipoe/sesta-rima/lang/reader.rkt
+++ racket/ipoe/sesta-rima/lang/reader.rkt
@@ -16,14 +16,13 @@
                 (* * *))
 #:extra-validator (lambda (s*)
                     (and
-                      ;; Word constraints (after position 0, want +1, +3, +4, +1, +2
+                      ;; Word constraints (after position 0, want +1, +3, +4, +1, +2)
                       (for/and ([l+s* (in-list '(((0 . 0) (1 . 1) (3 . 2) (4 . 3) (2 . 4) (5 . 5))
                                                  ((1 . 0) (3 . 1) (4 . 2) (2 . 3) (5 . 4) (0 . 5))
                                                  ((2 . 0) (5 . 1) (0 . 2) (1 . 3) (3 . 4) (4 . 5))
                                                  ((3 . 0) (4 . 1) (2 . 2) (5 . 3) (0 . 4) (1 . 5))
                                                  ((4 . 0) (2 . 1) (5 . 2) (0 . 3) (1 . 4) (3 . 5))
                                                  ((5 . 0) (0 . 1) (1 . 2) (3 . 3) (4 . 4) (2 . 5))))])
-                        ;;(printf "Checking ~a\n" l+s*)
                         (apply word=? (map (lambda (l+s) (last-word (line (car l+s) (stanza (cdr l+s) s*)))) l+s*)))
                       ;; Tercet constraints.
                       ;; A specific pair of words must appear in each line the second
