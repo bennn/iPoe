ea60fc970eced5118f577eac733f0f6b7a5b4e7f
ben <types@ccs.neu.edu>
2015-08-27 02:38:16 -0400

    [options] stub file for parameters

---
+++ racket/ipoe/private/parameters.rkt
@@ -0,0 +1,19 @@
+#lang racket/base
+
+(provide
+  parameterize-from-hash
+)
+
+(require
+)
+
+;; =============================================================================
+
+(define (parameterize-from-hash H thunk)
+  'TODO)
+
+;; =============================================================================
+
+(module+ test
+
+)
