8b5eb0b56d3d3f2e682679272f318b80f345e87c
ben <types@ccs.neu.edu>
2015-08-28 02:45:29 -0400

    [racket/scrape] add merriam-webster

--- racket/ipoe/private/scrape/scrape-words.rkt
+++ racket/ipoe/private/scrape/scrape-words.rkt
@@ -105,8 +105,33 @@
        (and h2 (add1 (count-chars #\· h2)))))
 
     ;; src
-    'the-free-dictionary
-    ))
+    'the-free-dictionary))
+
+(define merriam-webster
+  (word-scraper
+    ;; word->url
+    (lambda (w) (string-append "http://www.merriam-webster.com/dictionary/" w))
+
+    ;; sxml->definition
+    (lambda (sxml)
+      (let ([p ((if-car-sxpath `(// p ,(class? "bottom_entry") *text*)) sxml)])
+        (and p (substring p 2))))
+
+    ;; sxml->num-syllables
+    (lambda (sxml)
+      ;; The <span> we get has a list of children:
+      ;; - a string for each syllable part
+      ;; - a separator between each syllable part
+      ;; - 2 metadata children, one to say "span" and the other for attributes
+      (let* ([span ((if-car-sxpath `(// span ,(class? "hw-syllables"))) sxml)]
+             [len  (length (or span '(1 2)))]
+             ;; Add 1 to quotient if len is odd
+             [q    (quotient len 2)])
+        (and (positive? q) q)))
+
+    ;; src
+    'merriam-webster))
+
 ;; -----------------------------------------------------------------------------
 ;(define TODO
 ;  (word-scraper
@@ -146,9 +171,17 @@
   ;  [not-a-word == #f]
   ;)
 
-  (check-apply* the-free-dictionary
-   ["penguin" == (word-result "penguin" "Any of various stout, flightless aquatic birds of the family Spheniscidae, of the Southern Hemisphere, having flipperlike wings and webbed feet adapted for swimming and diving, short scalelike feathers, and white underparts with a dark back." 2 'the-free-dictionary)]
-   ["boilerplate" == (word-result "boilerplate" "Steel in the form of flat plates used in making steam boilers." 3 'the-free-dictionary)]
-   [not-a-word == #f]
-  )
+  ;(check-apply* the-free-dictionary
+  ; ["penguin" == (word-result "penguin" "Any of various stout, flightless aquatic birds of the family Spheniscidae, of the Southern Hemisphere, having flipperlike wings and webbed feet adapted for swimming and diving, short scalelike feathers, and white underparts with a dark back." 2 'the-free-dictionary)]
+  ; ["boilerplate" == (word-result "boilerplate" "Steel in the form of flat plates used in making steam boilers." 3 'the-free-dictionary)]
+  ; [not-a-word == #f]
+  ;)
+
+  (check-apply* merriam-webster
+    ["penguin" == (word-result "penguin" "a black-and-white bird that cannot fly, that uses its wings for swimming, and that lives in or near the Antarctic" 2 'merriam-webster)]
+    ["candelabra" == (word-result "candelabra" "an object with several branches for holding candles or lights" 4 'merriam-webster)]
+    ["day" == (word-result "day" "the part of the day when people are usually most active and when most businesses are open" 1 'merriam-webster)]
+    ["umbrella" == (word-result "umbrella" "something that includes several or many different things" 3 'merriam-webster)]
+    [not-a-word == #f]
+   )
 )
