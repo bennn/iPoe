c5658e90de1e901a61fe5caa81f193e20efd3b76
ben <types@ccs.neu.edu>
2015-08-28 01:17:23 -0400

    [racket/scrape] reorganize scraper into a struct

--- racket/ipoe/private/scrape/scrape-words.rkt
+++ racket/ipoe/private/scrape/scrape-words.rkt
@@ -10,6 +10,7 @@
 
 (require
   racket/string
+  (only-in racket/match match-define)
   (only-in sxml if-car-sxpath)
   "scrape-util.rkt"
 )
@@ -17,39 +18,77 @@
 ;; =============================================================================
 
 (struct word-result (
-  word
-  definition
-  num-syllables
+  word ;; String, the result word
+  definition ;; String, a definition of the word
+  num-syllables ;; Natural, number of syllables in the word
+  src ;; Symbol, describes where this information came from
 ) #:transparent )
 
-(define DICTIONARY.COM "http://dictionary.reference.com")
 
 (define (scrape-word w)
-  (scrape-dictionary.com w))
+  (or (dictionary.com w)))
+  ;urban-dictionary
+  ;merriam-webster
+  ;the-free-dictionary
 
-;; Parse results from dictionary.com
-;; (: scrape-dictionary.com (-> String (U #f Word-Result)))
-(define (scrape-dictionary.com word)
-  (define url-str (string-append DICTIONARY.COM "/browse/" word))
-  (define sxml (url->sxml url-str))
-  (define def (dictionary.com-description sxml))
-  (define syl (dictionary.com-syllable sxml))
-  ;; If word is not found, just return #f
-  (and def syl (word-result word def syl)))
+;; -----------------------------------------------------------------------------
+;; Data Definition: Word Scraper
 
-;; (-> SXML String)
-(define (dictionary.com-description sxml)
-  (define div ((if-car-sxpath `(// div ,(class? "def-content"))) sxml))
-  ;; caddr is scary, but *text* seems to do caddr
-  (and div (string-trim (cadddr div))))
+(struct word-scraper (
+  word->url
+  ;; (-> String String)
+  ;; Build a URL query string from a word.
 
-;; Dictionary.com keeps a syllable-ized version of each word
-;; (-> SXML (U #f Natural))
-(define (dictionary.com-syllable sxml)
-  (define elem ((if-car-sxpath '(// h1 span @ data-syllable *text*)) sxml))
-  (and elem
-       (add1 (count-chars (lambda (c) (char=? c #\·)) elem))))
+  sxml->definition
+  ;; (-> SXML (U #f String))
+  ;; Parse the definition from a word's web page.
 
+  sxml->num-syllables
+  ;; (-> SXML (U #f Natural))
+  ;; Parse the number of syllables from a word's web page.
+
+  src
+  ;; Symbol
+  ;; The name of this scraper
+
+) #:property prop:procedure
+  ;; Search the web for information on a word.
+  ;; An instance of this struct defines the search protocol for 1 website
+  ;; (-> Word-Scraper String Word-Result)
+  (lambda (self word)
+    (match-define (word-scraper word->url
+                                sxml->definition
+                                sxml->num-syllables
+                                src) self)
+    (define url (word->url word))
+    (define sxml (url->sxml url))
+    (define def (sxml->definition sxml))
+    (define syl (sxml->num-syllables sxml))
+    (and def syl (word-result word def syl src)))
+)
+
+;; -----------------------------------------------------------------------------
+;; dictionary.com
+
+(define dictionary.com
+  (word-scraper
+    ;; word->url
+    (lambda (w) (string-append "http://dictionary.reference.com/browse/" w))
+
+    ;; sxml->definition
+    (lambda (sxml)
+      (let ([div ((if-car-sxpath `(// div ,(class? "def-content"))) sxml)])
+        ;; cadddr is scary, but *text* seems to be caddr
+        (and div (string-trim (cadddr div)))))
+
+    ;; sxml->num-syllables
+    (lambda (sxml)
+      (let ([h1 ((if-car-sxpath '(// h1 span @ data-syllable *text*)) sxml)])
+        (and h1
+             (add1 (count-chars (lambda (c) (char=? c #\·)) h1)))))
+
+    ;; src
+    'dictionary.com))
 
 ;; =============================================================================
 
@@ -72,8 +111,8 @@
    ["hguwisdvzodxv" == #f]
   )
 
-  (check-apply* scrape-dictionary.com
-   ["penguin" == (word-result "penguin" "any of several flightless, aquatic birds of the family Spheniscidae, of the Southern Hemisphere, having webbed feet and wings reduced to flippers." 2)]
-   ["flower" == (word-result "flower" "the blossom of a plant." 2)]
+  (check-apply* dictionary.com
+   ["penguin" == (word-result "penguin" "any of several flightless, aquatic birds of the family Spheniscidae, of the Southern Hemisphere, having webbed feet and wings reduced to flippers." 2 'dictionary.com)]
+   ["flower" == (word-result "flower" "the blossom of a plant." 2 'dictionary.com)]
   )
 )
