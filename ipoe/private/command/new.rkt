5edf6c406d645c33fbcc4ad10d1fb31bc6505d25
ben <types@ccs.neu.edu>
2015-09-20 19:14:40 -0400

    [cmd] new, for adding new words & poems

---
+++ ipoe/private/command/new.rkt
@@ -0,0 +1,96 @@
+#lang racket/base
+
+;; TBA
+;; Probably a uniform endpoint for
+;; - Registering new poem forms
+;; - Adding word
+
+;; TODO accept input from file? (lots of words to submit)
+
+(provide
+  new
+)
+
+;; -----------------------------------------------------------------------------
+
+(require
+  racket/cmdline
+  ipoe/private/db
+  ipoe/private/parameters
+  (only-in ipoe/private/ui read-natural get-user-input alert)
+  (only-in racket/string string-suffix?)
+)
+
+;; =============================================================================
+;; === API
+
+(define (new arg*)
+  (command-line
+   #:argv arg*
+   #:args (word-or-form)
+   (cond
+    [(rkt-file? word-or-form)
+     (new-form word-or-form)]
+    [else
+     (new-word word-or-form)])))
+
+;; -----------------------------------------------------------------------------
+
+;; Assumes that `fname` is a valid (existing) Racket file
+(define (new-form fname)
+  ;; Still unsure how to implement this,
+  ;; See the discussion on github:
+  ;;   https://github.com/bennn/iPoe/issues/10
+  (displayln "NEW FORM not implemented"))
+
+(define (new-word w)
+  ;; Init database
+  ;; Check if word exists
+  ;; Interactively add word (get syllables, get rhymes)
+  (parameterize-from-hash (options-init)
+    (lambda ()
+      (with-ipoe-db #:commit? #t
+                    #:user (*user*)
+                    #:dbname (*dbname*)
+                    #:interactive? #t
+        (lambda ()
+          (cond
+           [(word-exists? w)
+            (alert (format "Word '~a' already exists, not adding (for now you can't update the syllables or rhymes)" w))]
+           [(add-word w #:syllables (get-syllables w)
+                        #:interactive? #t)
+            (alert "Success!")]
+           [else
+            (alert (format "Failed to add word '~a'" w))]))))))
+
+;; -----------------------------------------------------------------------------
+;; --- util
+
+(define (get-syllables w)
+  ;; No description
+  (get-user-input read-natural
+                  #:prompt (format "How many syllables does '~a' have?" w)))
+
+(define (rkt-file? fname)
+  (and (string-suffix? fname ".rkt")
+       (file-exists? fname)))
+
+;; =============================================================================
+
+(module+ test
+  (require
+    rackunit
+    ipoe/private/rackunit-abbrevs)
+
+
+  ;; -- rkt-file?
+  (check-true* (lambda (p) (rkt-file? (path->string (make-resolved-module-path p))))
+    ["./new.rkt"]
+    ["./create.rkt"]
+    ["./../private.rkt"])
+
+  (check-false* rkt-file?
+    ["'a"]
+    ["1251"]
+    ["nope.txt"])
+)
