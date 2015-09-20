97c71716a5a387dcbbcb382a66830f3a28409a16
ben <types@ccs.neu.edu>
2015-09-20 00:55:46 -0400

    [init] add deps, command to info

--- ipoe/info.rkt
+++ ipoe/info.rkt
@@ -1,6 +1,9 @@
 #lang info
 
 (define collection 'use-pkg-name)
+(define deps '("base" "rackunit-lib"))
 (define pkg-desc "Family of languages for editing poetry")
 (define pkg-authors '(ben))
+(define raco-commands '(("ipoe" (submod ipoe/main main) "console UI" #f)))
+;(define scribblings '(("scribblings/ipoe.scrbl")))
 (define version "0.1")
--- ipoe/main.rkt
+++ ipoe/main.rkt
@@ -2,3 +2,20 @@
 
 ;; TBA
 ;; Maybe, tools for language builders. We'll see what we can abstract.
+
+;; =============================================================================
+
+(module+ main
+  (require racket/cmdline)
+  (command-line
+   #:program "ipoe"
+   #:args ANY*
+   (displayln "UNDER CONSTRUCTION"))
+)
+
+;; =============================================================================
+
+(module+ test
+  (require rackunit)
+
+)
