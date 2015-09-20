40a15096cf4023e1d3e3ddd6471ce2c19d869121
ben <types@ccs.neu.edu>
2015-09-19 22:02:40 -0400

    [spell] remove duplicate misspelling warnings

--- racket/ipoe/private/spellcheck.rkt
+++ racket/ipoe/private/spellcheck.rkt
@@ -17,6 +17,8 @@
   ipoe/private/parse
   ipoe/private/suggest
   ipoe/private/ui
+  (only-in racket/list remove-duplicates)
+  (only-in racket/set mutable-set set-add! set-member? set->list)
   (only-in racket/string string-split)
   (only-in racket/file file->value))
 
@@ -28,26 +30,38 @@
 ;; - only gives 1 suggestion (be nicer to display in a dropdown)
 ;; (: check-spelling (-> (Sequenceof String) Void))
 (define (check-spelling line*)
+  (define seen (mutable-set))
   (define misspelled*
-    (for/list
-              ([line line*]
+    (for/list ([line line*]
                [line-num (in-naturals)])
-      (for/list
-                ([w (in-list (string->word* line))]
+      (for/list ([w (in-list (string->word* line))]
                  [word-num (in-naturals)]
-                 #:when (not (word-exists? w)))
-        (define suggestions (suggest-spelling w #:limit 7))
-        (define suggest-str (if (null? suggestions) "" (format " Maybe you meant '~a'?" (car suggestions))))
-        (alert (format "Misspelled word '~a' on line '~a'.~a" w line-num suggest-str))
-        (cons w suggestions))))
+                 #:when (and (not (word-exists? w))
+                             (not (set-member? seen w))))
+          (define suggestions (suggest-spelling w #:limit 7))
+          (define s-str
+            (if (null? suggestions)
+                ""
+                (format " Maybe you meant '~a'?" (car suggestions))))
+          (alert (format "Misspelled word '~a' on line ~a.~a" w line-num s-str))
+          (set-add! seen w)
+          (cons w suggestions))))
   (if (null? (car misspelled*))
       (success 'check-spelling #t)
-      (failure 'check-spelling (apply append misspelled*))))
+      (failure 'check-spelling
+        (remove-duplicates #:key car
+          (apply append misspelled*)))))
 
 ;; =============================================================================
 
 (module+ test
   (require rackunit ipoe/private/rackunit-abbrevs)
+
+  (define (check-misspelled w* [len #f])
+    (check-print
+      (for/list ([i (in-range (or len (length w*)))])
+        #rx"^Misspelled word")
+      (lambda () (check-spelling w*))))
 
   ;; -- check-spelling
   (with-ipoe-db #:commit? #f (lambda ()
@@ -59,14 +73,17 @@
           [bad2 "uhnojfyondvwhbonvwf"]
           [bad3 "hjvndkwcxs"]
           [bad4 "xz"])
-      (check-apply* (lambda (w*)
-                      (check-print
-                        (for/list ([w (in-list w*)])
-                          #rx"^Misspelled word")
-                        (lambda () (check-spelling w*))))
+      (check-apply* check-misspelled
         [(list bad1) == (failure 'check-spelling (list (list bad1)))]
         [(list bad1 bad2 bad3) == (failure 'check-spelling (list (list bad1) (list bad2) (list bad3)))]
         [(list bad4) == (failure 'check-spelling (list (list bad4 "be" "of" "to" "a" "in" "I" "it")))]
       ))))
+
+  ;; -- check-spelling, remove duplicates
+  (with-ipoe-db #:commit? #f (lambda ()
+    (let* ([bw "jipaengva"]
+           [bw2 (string-append bw " " bw)])
+      (check-apply* (lambda (w*) (check-misspelled w* 1))
+       [(list bw2 bw2 bw2) == (failure 'check-spelling (list (list bw)))]))))
 
 )
