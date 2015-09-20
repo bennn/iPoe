869853b798240848f2e3675475a7a3a91310def1
ben <types@ccs.neu.edu>
2015-09-20 18:36:55 -0400

    [cmd/db] moved command-line out of private/ folder

--- ipoe/private/command/db.rkt
+++ ipoe/private/command/db.rkt
@@ -2,6 +2,8 @@
 
 ;; Command-line interface to the DB repl
 
+;; TODO it's time to get serious here and break this into helper functions
+
 (provide
   db
   ;; (-> '() Void)
@@ -12,18 +14,89 @@
 
 (require
   racket/cmdline
-  (only-in racket/system system)
+  ipoe/private/db
+  racket/match
 )
 
 ;; =============================================================================
 
-;; TODO this is AWFUL. Should:
-;; 1. require the main submodule from db.rkt
-;; 2. run that submodule
-;; NO RELATIVE PATHS
 (define (db arg*)
+  ;; -- parameters
+  (define commit? (make-parameter #f))
+  (define output-file (make-parameter #f))
+  ;; --
+  (define natural? exact-nonnegative-integer?)
+  (define (exit? s)
+    (memq s '(exit q quit)))
+  (define (skip n seq)
+    (for ([x seq] [m (in-range n)]) (void)))
+  (define (take n seq)
+    (for/list ([x seq] [m (in-range n)]) x))
+  ;; -- repl
   (command-line
    #:argv arg*
+   #:once-each
+    [("-c" "--commit") "Commit to database" (commit? #t)]
+    [("-o" "--output") o-p "Save interactions to file" (output-file o-p)]
    #:args ()
-   (system "racket ../db.rkt")))
+   (begin
+     (printf "Initializing DB connection & starting REPL ...\n")
+     (with-ipoe-db #:commit? (commit?)
+       (lambda ()
+         (let loop ()
+           (display "ipoe> ")
+           (match (read)
+            [(or (? eof-object?)
+                 (? exit?)
+                 (and (? list?) (? (lambda (x) (exit? (car x))))))
+             (printf "Goodbye\n")]
+            ['help
+             (displayln "Available commands:")
+             (for ([cmd (in-list '(id->word rhymes-with syllables->word*
+                                   word-exists? word->syllables word->rhyme*
+                                   word->almost-rhyme*))])
+               (printf "- ~a\n" cmd))
+             (loop)]
+            [(list 'id->word (? natural? n))
+             (displayln (id->word n))
+             (loop)]
+            [(list 'rhymes-with? (? string? sym1) (? string? sym2))
+             (displayln (rhymes-with? sym1 sym2))
+             (loop)]
+            [(list 'syllables->word* (? natural? n1) '#:limit (? natural? n2))
+             (displayln (take n2 (syllables->word* n1)))
+             (loop)]
+            [(list 'syllables->word* (? natural? n1) '#:limit (? natural? n2) '#:skip (? natural? n3))
+             (define s (syllables->word* n1))
+             (skip n3 s)
+             (displayln (take n2 s))
+             (loop)]
+            [(list 'word->almost-rhyme* (? string? w) '#:limit (? natural? n))
+             (displayln (take n (word->almost-rhyme* w)))
+             (loop)]
+            [(list 'word->almost-rhyme* (? string? w) '#:limit (? natural? n) '#:skip (? natural? n2))
+             (define s (word->almost-rhyme* w))
+             (skip n2 s)
+             (displayln (take n s))
+             (loop)]
+            [(list 'word->id (? string? w))
+             (displayln (word->id w))
+             (loop)]
+            [(list 'word->rhyme* (? string? w) '#:limit (? natural? n1))
+             (displayln (take n1 (word->rhyme* w)))
+             (loop)]
+            [(list 'word->rhyme* (? string? w) '#:limit (? natural? n1) '#:skip (? natural? n2))
+             (define s (word->rhyme* w))
+             (skip n2 s)
+             (displayln (take n1 s))
+             (loop)]
+            [(list 'word->syllables (? string? w))
+             (displayln (word->syllables w))
+             (loop)]
+            [(list 'word-exists? (? string? w))
+             (displayln (word-exists? w))
+             (loop)]
+            [x
+             (printf "Unknown command '~a'\n" x)
+             (loop)])))))))
 
