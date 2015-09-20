b0621232d741785a5b8becf9faa9d0b3c81236f3
ben <types@ccs.neu.edu>
2015-09-19 20:45:17 -0400

    [db] note: blank DB/USER skips the database

--- racket/ipoe/private/db.rkt
+++ racket/ipoe/private/db.rkt
@@ -107,12 +107,12 @@
 (define *connection*  (make-parameter #f))
 ;;(define *logfile* (make-parameter #f))
 
-(define DBNAME-PROMPT "Enter the name of your local ipoe database.")
+(define DBNAME-PROMPT "Enter the name of your local ipoe database (Leave blank to skip):")
 (define DBNAME-DESCRIPTION
   (string-append "Missing run-time parameter for ipoe database name." " "
                  "Please enter the database name."))
 
-(define USER-PROMPT "Enter your database username")
+(define USER-PROMPT "Enter your database username (Leave blank to skip):")
 (define USER-DESCRIPTION
   (string-append "Missing run-time parameter for database username." " "
                  "Please enter a username to connect to the ipoe database."))
