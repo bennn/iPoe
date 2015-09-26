981606e6a1db8428edd68c9b416e71f01242ff6f
ben <types@ccs.neu.edu>
2015-09-26 11:30:36 -0400

    [haiku] add some comments

--- examples/haiku/numbers.rkt
+++ examples/haiku/numbers.rkt
@@ -1,5 +1,7 @@
 #lang ipoe/haiku
 
+#:comment "This-is-a-'certified'-haiku.-Because-numbers-are-converted-to-English."
+
 1 2 3 4 5
 12000102
 860
--- ipoe/haiku/lang/reader.rkt
+++ ipoe/haiku/lang/reader.rkt
@@ -1,9 +1,12 @@
 #lang ipoe
 
-;; One stanza,
-;; no rhyme constraints
+;; 1 stanza, 3 lines
+;; No rhyme constraints
 ;; 5 syllables in first line
 ;; 7 in second
 ;; 5 in last
+
+;; Should be about #nature
+
 #:name haiku
 #:rhyme-scheme ((5 7 5))
