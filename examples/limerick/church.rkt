7c7cb2ca57d90daba7f7dc0b3f65da17ff3e35ca
ben <types@ccs.neu.edu>
2015-09-26 12:53:22 -0400

    [limerick] more examples #computable

---
+++ examples/limerick/church.rkt
@@ -0,0 +1,11 @@
+#lang ipoe/limerick
+
+#:source "http://www.oedilf.com/db/Lim.php?Word=Church-Turing%20thesis"
+#:poetic-license 3
+
+
+Each computer, in theory, is suitable
+To attack any problem computable.
+This thesis (Church Turing),
+Unproven, alluring,
+Remains, as we speak, irrefutable
---
+++ examples/limerick/dandruff.rkt
@@ -0,0 +1,7 @@
+#lang ipoe/limerick
+
+"You have dandruff," my skin doctor said,
+"Which are skin flakes -- large, oily, and dead."
+He prescribed some shampoo,
+And my social life grew.
+Since that day, I've been getting good head.
