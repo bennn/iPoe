751e4670ad2449d254ef54cfed578be188b41e8d
ben <types@ccs.neu.edu>
2015-09-29 17:50:01 -0400

    ** update info.rkt

--- ipoe/info.rkt
+++ ipoe/info.rkt
@@ -1,7 +1,8 @@
 #lang info
 
 (define collection 'use-pkg-name)
-(define deps '("base" "rackunit-lib"))
+(define deps '("base" "rackunit-lib" "sxml" "levenshtein"))
+(define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-desc "Family of languages for editing poetry")
 (define pkg-authors '(ben))
 (define raco-commands '(("ipoe" (submod ipoe/main main) "console UI" #f)))
