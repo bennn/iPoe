c009b344280597ea2cdaf7177fe4f1247d097f26
ben <types@ccs.neu.edu>
2015-10-04 14:20:47 -0400

    ** update to use #:constraint

--- ipoe/villanelle/lang/reader.rkt
+++ ipoe/villanelle/lang/reader.rkt
@@ -8,15 +8,15 @@
                 [A  B R2]
                 [A  B R1 R2]}
 #:syllables 10
-#:extra-validator (lambda (P)
-                    ;; All R1 lines must be equal,
-                    ;; and all R2 lines must be equal
-                    (append
-                      (line=? (line 0 (stanza 0 P))
-                              (line 2 (stanza 1 P))
-                              (line 2 (stanza 3 P))
-                              (line 2 (stanza 5 P)))
-                      (line=? (line 2 (stanza 0 P))
-                              (line 2 (stanza 2 P))
-                              (line 2 (stanza 4 P))
-                              (line 3 (stanza 5 P)))))
+;; All R1 lines must be equal,
+;; and all R2 lines must be equal
+#:constraint
+  (line=? (line 0 (stanza 0))
+          (line 2 (stanza 1))
+          (line 2 (stanza 3))
+          (line 2 (stanza 5)))
+#:constraint
+(line=? (line 2 (stanza 0))
+        (line 2 (stanza 2))
+        (line 2 (stanza 4))
+        (line 3 (stanza 5)))
