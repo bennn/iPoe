f6b4f8bfa270549a219caba5b6ebcc5e34c0c5e5
ben <benjaminlgreenman@gmail.com>
2016-03-08 00:19:02 -0500

    [private] checked extra constraints

--- ipoe/villanelle/lang/reader.rkt
+++ ipoe/villanelle/lang/reader.rkt
@@ -10,13 +10,13 @@
 #:syllables 10
 ;; All R1 lines must be equal,
 ;; and all R2 lines must be equal
-#:constraint
+#:constraint (lambda ()
   (line=? (line 0 (stanza 0))
           (line 2 (stanza 1))
           (line 2 (stanza 3))
-          (line 2 (stanza 5)))
-#:constraint
-(line=? (line 2 (stanza 0))
-        (line 2 (stanza 2))
-        (line 2 (stanza 4))
-        (line 3 (stanza 5)))
+          (line 2 (stanza 5))))
+#:constraint (lambda ()
+  (line=? (line 2 (stanza 0))
+          (line 2 (stanza 2))
+          (line 2 (stanza 4))
+          (line 3 (stanza 5))))
